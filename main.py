bc3a5bda60f1c3630957fc9c0624512c665ebd4e
Mike Alves <michelalvs@gmail.com>
2021-01-13 20:36:00 -0300

    Created ReadME file and made the exercises pg 36

---
+++ main.py
@@ -0,0 +1,40 @@
+#this is a print function, common to most programming languages.
+print("Hi, I'm Mike =D - This is a python program.")
+
+
+# this is a function printing a variable with no value, the compiler will complain
+# that this variable value is not defined.
+#print(a)
+
+print (2 + 2)
+
+
+print (2++2)
+
+# This argument is not valid as python does not allow
+# zeros to the left of the int.
+#print (02)
+
+print (2, 3)
+
+print ("How many seconds there are in 42 minutes and 42 seconds?")
+
+print()
+
+print (42 * 60 + 42)
+
+print()
+
+print("How many miles there are in 10 kilometers?")
+
+print()
+
+# A mile is equal to 1,61 kilometers.
+# First we initialize a variable containing the value of a Mile
+
+mile = 1.61
+
+# Now we multiply a mile by 10 and show its results on the screen.
+
+print (mile * 10)
+
