9fe06cb36e94b63ad57348f27398a33ab60bcae5
Mike Alves <michelalvs@gmail.com>
2021-01-20 10:47:41 -0300

    Created Entry widget to recieve user input

--- pyguiconcept/application.py
+++ pyguiconcept/application.py
@@ -1,23 +1,64 @@
 from tkinter import *
 class Application:
     def __init__(self, master=None):
-        self.widget1 = Frame(master) #Cria o widget1 e atribui ele ao nosso container top-level(master)
-        self.widget1.pack() #Faz o pack do widget1
-        self.msg = Label(self.widget1, text="Primeiro widget") # Cria uma item msg que atribui uma string a janela widget1
-        self.msg["font"] = ("Calibri", "9", "italic") # Configuracoes visuais da string
-        self.msg.pack() 
-        self.sair = Button(self.widget1) # cria um botao na widget1
-        self.sair["text"] = "Clique aqui" #texto do botao "sair"
-        self.sair["font"] = ("Calibri", "9") 
-        self.sair["width"] = 10 #tamanho do botao
-        self.sair["command"] = self.mudarTexto
-        self.sair.pack()
+        self.fontePadrao = ("Arial", "10")
+        self.primeiroContainer = Frame(master)
+        self.primeiroContainer["pady"] = 10
+        self.primeiroContainer.pack()
 
-    def mudarTexto(self):
-        if self.msg["text"] == "Primeiro widget":
-            self.msg["text"] = "O botão recebeu um clique"
+        self.segundoContainer = Frame(master)
+        self.segundoContainer["padx"] = 20
+        self.segundoContainer.pack()
+
+        self.terceiroContainer = Frame(master)
+        self.terceiroContainer["padx"] = 20
+        self.terceiroContainer.pack()
+
+        self.quartoContainer = Frame(master)
+        self.quartoContainer["pady"] = 20
+        self.quartoContainer.pack()
+
+        self.titulo = Label(self.primeiroContainer, text="dados do usuário")
+        self.titulo["font"] = ("Arial", "10", "bold")
+        self.titulo.pack()
+
+        self.nomeLabel = Label(self.segundoContainer, text="Nome", font=self.fontePadrao)
+        self.nomeLabel.pack(side=LEFT)
+
+        self.nome = Entry(self.segundoContainer)
+        self.nome["width"] = 30
+        self.nome["font"] = self.fontePadrao
+        self.nome.pack(side=LEFT)
+
+        self.senhaLabel = Label(self.terceiroContainer, text="Senha", font=self.fontePadrao)
+        self.senhaLabel.pack(side=LEFT)
+
+        self.senha = Entry(self.terceiroContainer)
+        self.senha["width"] = 30
+        self.senha["font"] = self.fontePadrao
+        self.senha["show"] = "*"
+        self.senha.pack(side=LEFT)
+
+        self.autenticar = Button(self.quartoContainer)
+        self.autenticar["text"] = "Autenticar"
+        self.autenticar["font"] = ("Calibri", "8")
+        self.autenticar["width"] = 12
+        self.autenticar["command"] = self.verificaSenha
+        self.autenticar.pack()
+
+        self.mensagem = Label(self.quartoContainer, text="", font=self.fontePadrao)
+        self.mensagem.pack()
+    
+    #Método para verificação de senha
+
+    def verificaSenha(self):
+        usuario = self.nome.get()
+        senha = self.senha.get()
+        if usuario == "usuariodevmedia" and senha == "dev":
+            self.mensagem["text"] = "Autenticado"
         else:
-            self.msg["text"] = "Primeiro widget"
+            self.mensagem["text"] = "Erro de autenticação"
+
 
 root = Tk()
 Application(root)
